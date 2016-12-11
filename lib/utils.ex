c895f26b96cb66478f793025c1c0e686f3015ac1
Brendan Bowidas <brendanbowidas@Brendans-MacBook-Pro.local>
2016-12-10 19:53:46 -0500

    set docker-machine env to allow the execution of docker commands within the app on behalf of the user

--- lib/utils.ex
+++ lib/utils.ex
@@ -17,8 +17,33 @@ defmodule Sip.Utils do
 
   def set_env(machine_name) do
     {res, _} =  System.cmd "docker-machine", ["env", machine_name]
-    String.replace res, "\n", ""
+    
     res
+    |> format_env_vars
+    |> System.put_env
+  end
+
+  @doc """
+    Format string from "docker-machine env" command into a
+    list of key-value tuples
+  """
+  defp format_env_vars(env_vars) do
+    env_vars
+    |> String.replace("export", "")
+    |> String.replace("\n", "")
+    |> String.replace("#", "")
+    |> KeyValueParser.parse
+    |> Enum.filter(&is_docker_var/1)
+    |> Enum.map(fn({k, v}) ->
+       {Atom.to_string(k), v}
+     end)
+  end
+
+# Filter out keys that don't begin with "DOCKER"
+  defp is_docker_var({key, _val}) do
+    key
+    |> Atom.to_string
+    |> String.starts_with?("DOCKER")
   end
 
 end
--- mix.exs
+++ mix.exs
@@ -28,6 +28,9 @@ defmodule Sip.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:poison, "~> 3.0"}]
+    [
+      {:poison, "~> 3.0"},
+      {:key_value_parser, "~> 1.0"}
+    ]
   end
 end
